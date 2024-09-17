71d5dd5fb01ebad42991cc59090882e3be41b638
AnNguyen1998 <conandold@gmail.com>
2024-09-17 10:27:04 +0700

    first commit

---
+++ src/main/java/project/source/NewGemApplication.java
@@ -0,0 +1,13 @@
+package project.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewGemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/project/source/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package project.source.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/home")
+    public String homePage(){
+        return "Home Page";
+    }
+}
