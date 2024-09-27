da05f5f227af51fd76c702caa2d640e7771ed456
DPLam5665 <lamducphat5665@gmail.com>
2024-09-27 11:12:00 +0700

    swagger summary

--- src/main/java/project/source/controllers/HomeController.java
+++ src/main/java/project/source/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package project.source.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
+    @Operation(
+            method = "GET",
+            summary = "Get the home page UI",
+            description = "Send a request to get the home page :D")
     @GetMapping("/home")
     public String homePage(){
         return "Home Page";
--- src/main/java/project/source/controllers/HotelController.java
+++ src/main/java/project/source/controllers/HotelController.java
@@ -36,8 +36,8 @@ public class HotelController {
 
     @Operation(
             method = "GET",
-            summary = "Get all the hotels",
-            description = "Send a request to get all the hotel")
+            summary = "Get all the hotels (might need admin authentication)",
+            description = "Send a request to get all the hotel data ")
     @GetMapping("/allHotel")
     public ResponseEntity<ApiResponse> getAllHotel(@RequestParam(value = "page", defaultValue = "0")int page
                                         ,@RequestParam(value = "size", defaultValue = "8")int size){
@@ -58,7 +58,7 @@ public class HotelController {
 
     @Operation(
             method = "GET",
-            summary = "Get hotel by id",
+            summary = "Get hotel by id (might need admin authentication)",
             description = "Send a request to get the hotel with the path variable id")
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse> getHotelDetail(@PathVariable(value = "id")Long id){
@@ -71,7 +71,10 @@ public class HotelController {
         return ResponseEntity.ok(apiResponse);
     }
 
-
+    @Operation(
+            method = "GET",
+            summary =  "Get hotel with address, prices and page size (might need admin authentication)",
+            description = "Send a request to get the hotel data with corresponding parameters ('City', 'minPrice', 'maxPrice' and 'totalPage') ")
     @GetMapping("/search")
     public ResponseEntity<ApiResponse> searchHotel(@RequestParam(name = "city", required = false) City city,
                                                    @RequestParam(name = "min", required = false) BigDecimal minPrice,
@@ -93,6 +96,10 @@ public class HotelController {
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get the total number of hotels exist in the database (need admin authentication)",
+            description = "Send a request to get the value of 'TotalHotels'")
     @GetMapping("/totalHotel")
     public ResponseEntity<ApiResponse> getTotalHotels(){
         return ResponseEntity.ok(ApiResponse.builder()
@@ -101,7 +108,10 @@ public class HotelController {
                         .data(hotelService.getTotalHotels())
                 .build());
     }
-
+    @Operation(
+            method = "POST",
+            summary = "Post the hotel body to create a new hotel object",
+            description = "Send a request with hotel body ('name', 'location', 'city') to build and save a new object for class 'Hotel', respond with a rejecting message if not valid" )
     @PostMapping("/createHotel")
     public ResponseEntity<ApiResponse> createHotel(@Valid @RequestBody HotelDTO hotelDTO, BindingResult result){
         if(result.hasErrors()){
@@ -122,6 +132,11 @@ public class HotelController {
             return ResponseEntity.ok(apiResponse);
         }
     }
+
+    @Operation(
+            method = "PUT",
+            summary = "Put the new hotel body to an existed hotel",
+            description = "Send a request to find and save a new body ('name', 'location') for a targeted 'hotelId', respond with a rejecting message if not valid" )
     @PutMapping("/updateHotel/{id}")
     public ResponseEntity<ApiResponse> updateHotel(@Valid @RequestBody HotelDTO hotelDTO, @PathVariable(value = "id")Long id,
                                                    BindingResult result){
@@ -148,6 +163,10 @@ public class HotelController {
         }
     }
 
+    @Operation(
+            method = "PATCH",
+            summary = "Patch an existed hotel to disable it",
+            description = "Send a request to find and nullify the data of a targeted 'hotelId' " )
     @PatchMapping("/disable/{id}")
     public ResponseEntity<ApiResponse> disableHotel(@PathVariable(value = "id")Long id){
         hotelService.disableHotel(id);
@@ -159,6 +178,10 @@ public class HotelController {
         return ResponseEntity.ok(apiResponse);
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get hotel with the corresponding name ",
+            description = "Send a request to get the hotel data with corresponding parameters ('name', 'totalPages')  ")
     @GetMapping("/searchHotelByName")
     public ResponseEntity<ApiResponse> searchByNameHotel(@RequestParam(value = "name", defaultValue = "%")String name,
                                                          @RequestParam(value = "page",defaultValue = "0")int page,
